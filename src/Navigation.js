c20a3d6543105f8db8d3ab28bdc07ca616f17b2e
hhaslam11 <kalebmarc@hotmail.com>
2019-10-26 23:17:42 -0700

    add basic navbar component

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,48 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 
 import "./Navigation.scss";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  navbar: {
+    backgroundColor: 'red'
+  },
+  title: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    color: 'white'
+  }
+}));
 
 /**
  * @param {type} props.value description
  */
 export default function Map(props) {
+  const classes = useStyles();
+
   return (
-    <h1>Navbar Test</h1>
-  )
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar className={classes.navbar} >
+          <Typography variant="h6" className={classes.title}>
+            Owl
+          </Typography>
+
+          <IconButton edge="start" className={classes.menuButton}>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
