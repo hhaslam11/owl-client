15c9639ed74eb21d8178989de9aab062774547e9
hhaslam11 <kalebmarc@hotmail.com>
2019-10-29 02:47:22 -0700

    pass values to onChange callbacks & added full demo for Login component

--- src/Login.js
+++ src/Login.js
@@ -21,20 +21,23 @@ const useStyles = makeStyles(({
 }));
 
 /**
+ * @param {boolean} props.open is the modal visible or not
  * @param {string} props.emailValue fill email form with a value
  * @param {string} props.passwordValue fill password form with a value
  * @param {string} props.emailError error message for invalid email
  * @param {string} props.passwordError error message for invalid password
  * @param {string} props.generalError general error message if something went wrong
- * @param {function} props.emailOnChange callback function for when email gets changed (usually to clear error messages)
- * @param {function} props.passwordOnChange callback function for when password gets changed (usually to clear error messages)
+ * @param {function} props.emailOnChange callback function for when email gets changed (value is passed as argument)
+ * @param {function} props.passwordOnChange callback function for when password gets changed (value is passed as argument)
+ * @param {function} props.onSumbit callback function for when the submit button is clicked
+ * @param {function} props.onCancel callback function for when the cancel button is clicked
  */
 export default function Map(props) {
   const classes = useStyles();
 
   return (
     <>
-      <Dialog open={true} aria-labelledby="form-dialog-title">
+      <Dialog open={props.open} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">Login</DialogTitle>
 
         <DialogContent>
@@ -45,7 +48,7 @@ export default function Map(props) {
               id="email"
               type="email"
               value={props.emailValue}
-              onChange={props.emailOnChange}
+              onChange={value => props.emailOnChange(value.target.value)}
               aria-describedby="email-error-text"
             />
             {props.emailError && <FormHelperText id="email-error-text">{props.emailError}</FormHelperText>}
@@ -57,18 +60,18 @@ export default function Map(props) {
               id="password"
               type="password"
               value={props.passwordValue}
-              onChange={props.passwordOnChange}
+              onChange={value => props.passwordOnChange(value.target.value)}
               aria-describedby="password-error-text"
             />
             {props.passwordError && <FormHelperText id="password-error-text">{props.passwordError}</FormHelperText>}
           </FormControl>
         </DialogContent>
 
-        <DialogActions className={classes.root}>
-          <Button className={classes.root}>
+        <DialogActions>
+          <Button onClick={props.onCancel}>
             Cancel
           </Button>
-          <Button className={classes.root}>
+          <Button onClick={props.onSubmit}>
             Login
           </Button>
         </DialogActions>
--- src/stories/Login.stories.js
+++ src/stories/Login.stories.js
@@ -3,15 +3,15 @@ import { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Login from '../Login'
+import { Button } from '@material-ui/core';
 
 storiesOf('Login', module)
-  .add('Login', () => <Login/>)
-  .add('with email error', () => <Login emailError='Email address not found' />)
-  .add('with password error', () => <Login passwordError='Password is incorrect' />)
-  .add('with general error', () => <Login generalError='Email or password is incorrect' />)
-  .add('with filled values', () => <Login emailValue='CoolOwl@example.com' passwordValue='Hunter2' />)
+  .add('Login', () => <Login open={true}/>)
+  .add('with email error', () => <Login open={true} emailError='Email address not found' />)
+  .add('with password error', () => <Login open={true} passwordError='Password is incorrect' />)
+  .add('with general error', () => <Login open={true} generalError='Email or password is incorrect' />)
+  .add('with filled values', () => <Login open={true} emailValue='CoolOwl@example.com' passwordValue='Hunter2' />)
   .add('with email and password change', () => {
-
     const [state, setState] = useState({
       passwordError: 'Password cannot be empty!',
       emailError: 'Email cannot be empty!'
@@ -19,11 +19,72 @@ storiesOf('Login', module)
 
     return (
       <Login
+        open={true}
         emailError={state.emailError}
         emailOnChange={() => {setState({...state, emailError: null})}}
         passwordError={state.passwordError}
         passwordOnChange={() => {setState({...state, passwordError: null})}}
       />
+    )
+  })
+  .add('with open and close', () => {
+    const [state, setState] = useState({
+      open: false
+    });
+
+    return (
+      <>
+      <Button onClick={() => {setState({ ...state, open: true })}}>Login</Button>
+      <Login
+        open={state.open}
+        onCancel={() => setState({...state, open: false})}
+      />
+      </>
+    )
+  })
+  .add('full demo', () => {
+    const [state, setState] = useState({
+      email: '',
+      password: '',
+
+      emailError: '',
+      passwordError: '',
+
+      open: false
+    });
+    
+    const validate = (email, password) => {
+      if (!email) {
+        setState({...state, emailError: 'email cannot be blank'});
+        return;
+      }
+      if (!password) {
+        setState({...state, passwordError: 'password cannot be blank'});
+        return;
+      }
+      setState({ ...state, email: null, password: null, open: false });
+    };
+
+    return (
+      <>
+      <Button onClick={() => {setState({ ...state, open: true })}}>Login</Button>
+      <Login
+        open={state.open}
+
+        emailValue={state.email}
+        emailOnChange={(value) => setState({...state, email: value, emailError: null})}
+        emailError={state.emailError}
+
+        passwordValue={state.password}
+        passwordOnChange={(value) => setState({...state, password: value, passwordError: null})}
+        passwordError={state.passwordError}
+
+        onCancel={() => setState({...state, open: false})}
+        onSubmit={() => validate(state.email, state.password)}
+      />
+      <p>email: {state.email}</p>
+      <p>password: {state.password}</p>
+      </>
     )
   });
 
