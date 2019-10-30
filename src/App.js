e8dd53750e9c5427fffc17e9f45f3ff73ce75b4b
hhaslam11 <kalebmarc@hotmail.com>
2019-10-30 02:31:32 -0700

    seperate app from main page (render landing page if not logged in)

--- src/App.js
+++ src/App.js
@@ -3,93 +3,18 @@ import { useState } from 'react';
 
 import './App.scss';
 
-//Material UI components
-import MenuList from '@material-ui/core/MenuList';
-import MenuItem from '@material-ui/core/MenuItem';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import PublicIcon from '@material-ui/icons/Public';
+import Dashboard from './Dashboard';
+import Landing from './Landing';
 
-
-import Navigation from './Navigation';
-import Sidebar from './Sidebar';
-import Map from './Map';
-
-
-const navMenu = (
-  <MenuList>
-    <MenuItem>Inbox</MenuItem>
-    <MenuItem>Post Office</MenuItem>
-    <MenuItem>Logout</MenuItem>
-  </MenuList>
-)
-
-function App() {
-
+export default function App() {
   const [state, setState] = useState({
-    open: false,
-    selected: null,
-    countryName: null
+    loggedIn: false,
+    userId: null
   });
-
-  const onCountryClick = event => {
-    setState({
-      ...state,
-      selected: event.target.dataItem.dataContext.id,
-      countryName: event.target.dataItem.dataContext.name
-    });
-  };
-
-  const countryInfo = (
-    <div
-      role="presentation"
-    >
-      <List>
-        <ListItem>
-          <ListItemIcon><PublicIcon /></ListItemIcon>
-          <ListItemText primary={state.countryName} />
-        </ListItem>
-      </List>
-      <Divider />
-      <List>
-        <ListItem button>
-          <ListItemIcon><MailIcon /></ListItemIcon>
-          <ListItemText primary="Send letter" />
-        </ListItem>
-      </List>
-    </div>
-  )
-
+  
   return (
     <>
-      <Navigation
-        title="Owl"
-        menuList={navMenu}
-        backgroundColor="#012b54"
-      />
-      <Sidebar 
-        isOpen={state.selected}
-        displayUnderNavigation
-        onClose={() => {setState({ ...state, selected: null, countryName: null })}}
-        listItems={countryInfo}
-        width="250px"
-        backgroundColor="#f2f0f0"
-      />
-      <Map
-        color="#358c4b"
-        colorOnHover="#286b39"
-        onCountryClick={onCountryClick}
-        data={[{
-          id: state.selected,
-          fill: "#286b39"
-        }]}
-      />
+      {state.loggedIn ? <Dashboard/> : <Landing/>}
     </>
   );
 }
-
-export default App;
---
+++ src/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+import "./Landing.scss"
+
+export default function Landing() {
+  return (
+    <>
+      <h1>Landing page</h1>
+    </>
+  )
+}
