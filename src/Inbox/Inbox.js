d6c490f3c80d1055eea46d71414dc660ee4dc62d
hhaslam11 <kalebmarc@hotmail.com>
2019-11-02 01:24:22 -0700

    plug Inbox into the backend

--- src/Inbox/Inbox.js
+++ src/Inbox/Inbox.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 // Material ui
 import List from '@material-ui/core/List';
@@ -10,67 +11,12 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import Sidebar from '../Sidebar';
 import InboxListItem from '../Inbox/InboxListItem';
-import { TextareaAutosize, Button } from "@material-ui/core";
+import { TextareaAutosize, Button, CircularProgress, Grid } from "@material-ui/core";
 
 import "./Inbox.scss"
+import axios from "axios";
 
-//This is just driver data, it will eventually be
-//generated based off api data
-const list = (
-  <List>
-    <InboxListItem 
-      username="CoolOwl"
-      country="Burundi"
-      flag="https://restcountries.eu/data/bdi.svg"
-      unread
-    />
-    <InboxListItem 
-      username="hhaslam11"
-      country="Canada"
-      flag="https://restcountries.eu/data/can.svg"
-      unread
-    />
-    <InboxListItem 
-      username="Grid1985"
-      country="Mauritania"
-      flag="https://restcountries.eu/data/mrt.svg"
-      unread
-    />
-    <InboxListItem 
-      username="Abong1965"
-      country="Czechia"
-      flag="https://restcountries.eu/data/cze.svg"
-    />
-    <InboxListItem 
-      username="Alaines"
-      country="Nepal"
-      flag="https://restcountries.eu/data/npl.svg"
-    />
-    <InboxListItem 
-      username="Aries"
-      country="Germany"
-      flag="https://restcountries.eu/data/deu.svg"
-    />
-    <InboxListItem 
-      username="Thiect"
-      country="Austria"
-      flag="https://restcountries.eu/data/aut.svg"
-    />
-    <InboxListItem 
-      username="Shapid"
-      country="Chile"
-      flag="https://restcountries.eu/data/chl.svg"
-    />
-    <InboxListItem 
-      username="ChloePrice2"
-      country="Cook Islands"
-      flag="https://restcountries.eu/data/cok.svg"
-    />
-  </List>
-)
-//END DRIVER DATA
-//🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉🦉
-
+const API_SERVER = '//192.168.88.49:3000';
 const drawerWidth = '300px';
 
 const useStyles = makeStyles(theme => ({
@@ -98,8 +44,47 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+const getData = id => {
+  return axios.get(`${API_SERVER}/users/${id}/letters`)
+    .then(res => {
+      const received_letters = res.data.data.received_letters;
+      // const sent_letters     = res.data.data.sent_letters;
+
+      const inboxList = received_letters.map(el => (
+        <InboxListItem 
+          username={el.sender.username}
+          country={el.sender.country.name}
+          flag={el.sender.country.flag_image}
+          unread={!el.read}
+        />
+      ));
+      
+      return (
+        <List>
+          {inboxList}
+        </List>
+      )
+    });
+};
+
 export default function Inbox(props) {
   const classes = useStyles();
+
+  const [state, setState] = useState((
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      style={{height: "100%"}}
+    >
+      <CircularProgress/>
+    </Grid>
+  ))
+
+  useEffect(() => {
+    getData(81).then(a => setState(a));
+  }, []);
 
   return (
     <div className={classes.root}>
@@ -108,7 +93,7 @@ export default function Inbox(props) {
       <div style={{width: drawerWidth}}>
         <Sidebar
           permanent
-          listItems={list}
+          listItems={state}
           anchor="left"
           width={drawerWidth}
         />
