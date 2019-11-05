713086d7ca227b6591c30e8799a6391ef6dd0727
hhaslam11 <kalebmarc@hotmail.com>
2019-11-04 21:41:44 -0800

    remove entire Inbox component from stories

--- src/stories/Inbox.stories.js
+++ src/stories/Inbox.stories.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Inbox from '../Inbox/Inbox';
 import InboxListItem from '../Inbox/InboxListItem';
 import SidebarLoading from '../Inbox/SidebarLoading';
 import SidebarEmpty from '../Inbox/SidebarEmpty';
 
 storiesOf('Inbox', module)
-  .add('Inbox', () => <Inbox />)
   .add('Sidebar loading', () => <SidebarLoading />)
   .add('Sidebar Empty', () => <SidebarEmpty />)
   .add('Inbox List Item', () => <InboxListItem username="Brian" country="Canada"  flag="https://restcountries.eu/data/can.svg" />)
