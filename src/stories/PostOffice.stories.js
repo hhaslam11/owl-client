fd4089e0f3072c5fdf99010a7c717a920a54392f
hhaslam11 <kalebmarc@hotmail.com>
2019-11-05 19:32:04 -0800

    delete postoffice story

--- src/stories/PostOffice.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import PostOffice from '../PostOffice/PostOffice';
-
-storiesOf('Post Office', module)
-  .add('Post Office', () => <PostOffice/>);
