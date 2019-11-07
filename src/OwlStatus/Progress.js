f12ccdccf6702a641d31165250cb8a08d7211ba4
hhaslam11 <kalebmarc@hotmail.com>
2019-11-07 00:37:55 -0800

    i dont know what im doing but im commiting this before changing it all

--- src/OwlStatus/Progress.js
+++ src/OwlStatus/Progress.js
@@ -4,12 +4,35 @@ import React, { useState } from "react";
 import ProgressBar from 'react-bootstrap/ProgressBar'
 
 import './OwlStatus.scss';
+import axios from "axios";
+
+const API_SERVER = process.env.REACT_APP_API_SERVER;
+
+const getProgress = (userId) => {
+  axios.get(`${API_SERVER}/users/${userId}/owls`)
+    .then(res => {
+      const current = Date.now();
+      let delivery = null;
+      let sent = null;
+
+      for (const letter of res.data.data[0].letters) {
+        delivery = letter.delivery_date.getTime();
+        sent = letter.sent_date.getTime();
+        if ()
+        if (new Date(letter.delivery_date).getTime() <= Date.now()) return false
+      }
+      return true;
+    })
+}
 
 export default function Progress() {
 
   const [state, setState] = useState(1);
 
-  setTimeout(() => setState(state + 1), 1000);
+  // setTimeout(() => setState(state + 1), 1000);
+  getProgress(499);
+
+
   return (
     <>
     <ProgressBar animated now={state} />
